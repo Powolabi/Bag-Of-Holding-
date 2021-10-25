688d2239b3e3fc6b81c8ed4b2bbabe8469bd3114
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-10-25 13:57:38 -0700

    create item class and currency class to be updated with new and removed items/currency

---
+++ src/main/java/bsu/cs222/edu/calcStats.java
@@ -0,0 +1,8 @@
+package bsu.cs222.edu;
+
+public class calcStats {
+
+    public static void generateStats(){
+
+    }
+}
---
+++ src/main/java/bsu/cs222/edu/experiencePoints.java
@@ -0,0 +1,5 @@
+package bsu.cs222.edu;
+
+public class experiencePoints {
+    public static int xp = 0;
+}
