08a7c9c5f4e06be9dba97770c89e298e56e5e6aa
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-10-25 12:15:26 -0700

    Add Node classes

---
+++ src/main/java/bsu/cs222/edu/skills.java
@@ -0,0 +1,32 @@
+package bsu.cs222.edu;
+
+public class skills {
+    public int proficiency = 5;
+
+    // strength based
+    public int athletics;
+    // dex based
+    public int acrobatics;
+    public int sleightOfHand;
+    public int stealth;
+    // intelligence based
+    public int arcana;
+    public int history;
+    public int investigation;
+    public int nature;
+    public int religion;
+    // wisdom
+    public int animalHandling;
+    public int insight;
+    public int medicine;
+    public int perception;
+    public int survival;
+    // charisma
+    public int deception;
+    public int intimidation;
+    public int performance;
+    public int persuasion;
+
+
+
+}
