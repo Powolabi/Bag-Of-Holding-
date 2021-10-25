5a19cad5926fc2252dbd690d3800f08e3abd973c
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-10-25 09:47:02 -0700

    Begin github branch to class folder

---
+++ src/main/java/bsu/cs222/edu/randomRoll.java
@@ -0,0 +1,36 @@
+package bsu.cs222.edu;
+
+public class randomRoll {
+
+    public static int rollOfFour(){
+        int four;
+
+        return four;
+    }
+
+    public static int rollOfSix(){
+        int six;
+
+        return six;
+    }
+    public static int rollOfEight(){
+        int eight;
+
+        return eight;
+    }
+    public static int rollOfTen(){
+        int ten;
+
+        return ten;
+    }
+    public static int rollOfTwelve(){
+        int twelve;
+
+        return twelve;
+    }
+    public static int rollOfTwenty(){
+        int twenty;
+
+        return twenty;
+    }
+}
