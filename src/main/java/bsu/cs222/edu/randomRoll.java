a61ace7a55cb1233e1214ce208f6edcf5a404771
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-10-25 14:42:50 -0700

    create item class and currency class to be updated with new and removed items/currency

--- src/main/java/bsu/cs222/edu/randomRoll.java
+++ src/main/java/bsu/cs222/edu/randomRoll.java
@@ -6,37 +6,31 @@ public class randomRoll {
     // random value between 1 and 4
     public static int rollOfFour(){
         Random rand = new Random();
-
         return rand.nextInt(4) + 1;
     }
     // random value between 1 and 6
     public static int rollOfSix(){
         Random rand = new Random();
-
         return rand.nextInt(6) + 1;
     }
     // random value between 1 and 8
     public static int rollOfEight(){
         Random rand = new Random();
-
         return rand.nextInt(8) + 1;
     }
     // random value between 1 and 10
     public static int rollOfTen(){
         Random rand = new Random();
-
         return rand.nextInt(10) + 1;
     }
     // random value between 1 and 12
     public static int rollOfTwelve(){
         Random rand = new Random();
-
         return rand.nextInt(12) + 1;
     }
     // random value between 1 and 20
     public static int rollOfTwenty(){
         Random rand = new Random();
-
         return rand.nextInt(20) + 1;
     }
 }
