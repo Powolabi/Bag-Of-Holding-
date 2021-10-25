060be46122d724e2e3b29db49ae682d6d30f5a3d
KingSpike <brendanrmaygaming>
2021-10-25 09:31:38 -0700

    Begin github branch to class folder

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module bsu.cs222.edu {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens bsu.cs222.edu to javafx.fxml;
+    exports bsu.cs222.edu;
+}
