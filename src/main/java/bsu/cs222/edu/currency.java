0ba37a2efddc5228daab90daa4c6ab01dcabdcb3
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-10-25 13:04:39 -0700

    create item class and currency class to be updated with new and removed items/currency

--- src/main/java/bsu/cs222/edu/currency.java
+++ src/main/java/bsu/cs222/edu/currency.java
@@ -1,4 +1,40 @@
 package bsu.cs222.edu;
 
 public class currency {
+    public int copper = 0;
+    public int silver = 0;
+    public int gold = 0;
+    public int platinum = 0;
+
+    public void addCopper(int copperAdded){
+        this.copper += copperAdded;
+    }
+
+    public void addSilver(int silverAdded){
+        this.silver += silverAdded;
+    }
+
+    public void addGold(int goldAdded){
+        this.gold += goldAdded;
+    }
+
+    public void addPlatinum(int platinumAdded){
+        this.platinum += platinumAdded;
+    }
+
+    public void subtractCopper(int copperSubtracted){
+        this.copper -= copperSubtracted;
+    }
+
+    public void subtractSilver(int silverSubtracted){
+        this.silver -= silverSubtracted;
+    }
+
+    public void subtractGold(int goldSubtracted){
+        this.silver -= goldSubtracted;
+    }
+
+    public void subtractPlatinum(int platinumSubtracted){
+        this.platinum -= platinumSubtracted;
+    }
 }
