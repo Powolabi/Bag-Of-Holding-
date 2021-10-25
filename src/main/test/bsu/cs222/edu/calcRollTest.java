a2ea2d55dc5b6df9455529774c67727a744073d9
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-10-25 15:49:47 -0700

    create item class and currency class to be updated with new and removed items/currency

--- src/main/test/bsu/cs222/edu/calcRollTest.java
+++ src/main/test/bsu/cs222/edu/calcRollTest.java
@@ -1,7 +1,57 @@
 package bsu.cs222.edu;
 
 public class calcRollTest {
-    public static void randomRollTest(){
+    public static int rollOfFourTest() throws Exception{
+        int test = randomRoll.rollOfFour();
+        if (test >= 1 || test <= 4){
+            return 1;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 
+    public static int rollOfSixTest() throws Exception{
+        int test = randomRoll.rollOfSix();
+        if (test >= 1 || test <= 6){
+            return 1;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static int rollOfEightTest() throws Exception{
+        int test = randomRoll.rollOfEight();
+        if (test >= 1 || test <= 8){
+            return 1;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static int rollOfTenTest() throws Exception{
+        int test = randomRoll.rollOfTen();
+        if (test >= 1 || test <= 10){
+            return 1;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static int rollOfTwelveTest() throws Exception{
+        int test = randomRoll.rollOfTwelve();
+        if (test >= 1 || test <= 12){
+            return 1;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static int rollOfTwentyTest() throws Exception{
+        int test = randomRoll.rollOfTwenty();
+        if (test >= 1 || test <= 20){
+            return 1;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
