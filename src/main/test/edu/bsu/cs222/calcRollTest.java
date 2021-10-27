ea5772215e7ed2eae9b3ba712e0cf7baf3b1cdcd
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-10-27 10:45:23 -0700

    test for random roll created

--- src/main/test/edu/bsu/cs222/calcRollTest.java
+++ src/main/test/edu/bsu/cs222/calcRollTest.java
@@ -5,32 +5,32 @@ import org.junit.jupiter.api.Test;
 
 public class calcRollTest {
     @Test
-    public void rollOfFourTest() throws Exception{
+    public void rollOfFourTest(){
         int test = randomRoll.rollOfFour();
         Assertions.assertTrue(test >= 1 && test <= 4);
     }
     @Test
-    public void rollOfSixTest() throws Exception{
+    public void rollOfSixTest(){
         int test = randomRoll.rollOfSix();
         Assertions.assertTrue(test >= 1 && test <= 6);
     }
     @Test
-    public void rollOfEightTest() throws Exception{
+    public void rollOfEightTest(){
         int test = randomRoll.rollOfEight();
         Assertions.assertTrue(test >= 1 && test <= 8);
     }
     @Test
-    public void rollOfTenTest() throws Exception{
+    public void rollOfTenTest(){
         int test = randomRoll.rollOfTen();
         Assertions.assertTrue(test >= 1 && test <= 10);
     }
     @Test
-    public void rollOfTwelveTest() throws Exception{
+    public void rollOfTwelveTest(){
         int test = randomRoll.rollOfTwelve();
         Assertions.assertTrue(test >= 1 && test <= 12);
     }
     @Test
-    public void rollOfTwentyTest() throws Exception{
+    public void rollOfTwentyTest(){
         int test = randomRoll.rollOfTwenty();
         Assertions.assertTrue(test >= 1 && test <= 20);
     }
