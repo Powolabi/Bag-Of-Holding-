7e4cf862b42d6c402f01a37743e1a839921ccf2c
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-10-27 10:24:16 -0700

    test for random roll created

--- src/main/java/edu/bsu/cs222/generateCharacter.java
+++ src/main/java/edu/bsu/cs222/generateCharacter.java
@@ -1,8 +1,21 @@
 package edu.bsu.cs222;
 
 public class generateCharacter {
-    public static void generateAbilityScores(){
+    public static int generateAbilityScore(){
+        int[] score = new int[0];
+        int num = 6;
+        int value = 0;
 
+        for(int i = 0; i < 4; i++){
+            score[i] = randomRoll.rollOfSix();
+            if (i < num){
+                num = i;
+            }
+        }
+        for(int i = 0; i < score.length; i++){
+            value += score[i];
+        }
+        return value-num;
     }
 
     public static void chooseRandomLevel(){
