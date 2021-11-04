9819bf0aeb1436bd9008e26f92a528f19e1a458a
mlharker <micah.harker@gmail.com>
2021-11-03 23:22:45 -0400

    Fixed pathing issue in readCSV class

--- src/main/java/edu/bsu/cs222/readCSV.java
+++ src/main/java/edu/bsu/cs222/readCSV.java
@@ -2,17 +2,21 @@ package edu.bsu.cs222;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class readCSV {
 
     public static void scanFile(){
-        Scanner sc;
-        try {
-            sc = new Scanner(new File("C:equipment.csv"));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        Scanner sc = null;
+        InputStream csv = Thread.currentThread().getContextClassLoader().getResourceAsStream("equipment.csv");
+        sc = new Scanner(csv);
+        sc.useDelimiter(",");
+        while (sc.hasNext()){
+            System.out.print(sc.next());
         }
+        sc.close();
 
     }
 
