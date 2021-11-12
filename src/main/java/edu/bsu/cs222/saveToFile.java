172861e83d19cb466884d5accbc013c1ca4f0b05
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-11-12 11:40:00 -0800

    Brendans newest commit, 11/12/2021 10:14am

--- src/main/java/edu/bsu/cs222/saveToFile.java
+++ src/main/java/edu/bsu/cs222/saveToFile.java
@@ -7,12 +7,19 @@ import java.io.IOException;
 public class saveToFile {
 
     public static void saveXtoY(String x, String y){
+
         try{
             FileWriter myObj = new FileWriter( "C:\\Users\\brend\\IdeaProjects\\Final-Project-Brendan-Peter-Micah\\src\\main\\resources\\" + y + ".txt");
             myObj.write(x);
         } catch(IOException e){
             e.printStackTrace();
         }
+
+    }
+    // break data, aka items, spells, and character information in to its individual parts,
+    // Convert them to string type, tie all like information.
+    // if its a new item or larger piece of data, create a new line.
+    public static void parseData(String toStore){
 
     }
 }
