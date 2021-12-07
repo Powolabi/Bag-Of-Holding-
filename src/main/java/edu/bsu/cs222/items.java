06ba276438f41cabb2572d907f3673070c3a98d3
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-12-07 15:25:17 -0800

    Brendans newest commit, 11/18/2021
    GUI redesign

--- src/main/java/edu/bsu/cs222/items.java
+++
@@ -1,5 +0,0 @@
-package edu.bsu.cs222;
-
-public class items {
-    // sorting algo of item.java data
-}
--- src/main/java/edu/bsu/cs222/main.java
+++ src/main/java/edu/bsu/cs222/main.java
@@ -13,7 +13,7 @@ public class main {
 
         int playerCount = numberOfPlayers(input);
 
-
+        createCharacter(playerCount);
 
     }
 
