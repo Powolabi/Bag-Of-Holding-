ca028986b24621e968bb2d2a59531c331238deb2
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-12-08 11:32:24 -0800

    Brendans newest commit, 11/18/2021
    GUI redesign

--- src/main/java/edu/bsu/cs222/pullFromFile.java
+++ src/main/java/edu/bsu/cs222/pullFromFile.java
@@ -24,6 +24,21 @@ public class pullFromFile {
             String line = sc.nextLine();
             String[] list = line.split(".");
             characterDetails details = new characterDetails(list[0], list[1], list[2], list[3], list[4], list[5], list[6], list[7], list[8], list[9], list[10], list[11], list[12]);
+            System.out.println("" + list[0]);
+            System.out.println("" + list[1]);
+            System.out.println("" + list[2]);
+            System.out.println("" + list[3]);
+            System.out.println("" + list[4]);
+            System.out.println("" + list[5]);
+            System.out.println("" + list[6]);
+            System.out.println("" + list[7]);
+            System.out.println("" + list[8]);
+            System.out.println("" + list[9]);
+            System.out.println("" + list[10]);
+            System.out.println("" + list[11]);
+            System.out.println("" + list[12]);
+
+
         }
     }
 
