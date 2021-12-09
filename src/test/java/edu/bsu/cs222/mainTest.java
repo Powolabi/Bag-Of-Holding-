f860ddff3d684394816db6e341d36bdb06cb0475
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-12-08 18:11:29 -0800

    Brendans newest commit, 11/18/2021
    GUI redesign

--- src/test/java/edu/bsu/cs222/mainTest.java
+++ src/test/java/edu/bsu/cs222/mainTest.java
@@ -3,9 +3,14 @@ package edu.bsu.cs222;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+
 public class mainTest {
     @Test
     public void testStart(){
-        
+        String route = "character";
+        System.setIn(new ByteArrayInputStream(route.getBytes()));
+        String name = "name";
     }
 }
