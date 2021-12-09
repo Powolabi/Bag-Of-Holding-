6bf6edbd1d82e772800a041cda51407eb36b5903
KingSpike <87199787+KingSpike@users.noreply.github.com>
2021-12-08 19:39:24 -0800

    RELEASE 0.3.0

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,16 +1,4 @@
 module edu.bsu.cs {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-    requires javafx.graphics;
-
-    opens edu.bsu.cs222 to javafx.fxml;
     exports edu.bsu.cs222;
 }
